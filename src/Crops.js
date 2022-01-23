40d7330b7a2bd77ee3c377ecb5ff938798a05a8e
onlyorganic <onlyorganic.io@gmail.com>
2022-01-23 18:48:41 +0530

    merge all vedios into one

--- src/Crops.js
+++ src/Crops.js
@@ -4,25 +4,34 @@ import Box from "@mui/material/Box"
 import Grid from "@mui/material/Grid"
 import Typography from "@mui/material/Typography"
 
+const getDays = (start, end) => {
+  const date1 = end ? new Date(end) : new Date()
+  const date2 = new Date(start)
+  const diffTime = Math.abs(date2 - date1)
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
+  return diffDays + " days"
+}
 const crops = [
   {
     id: "paddy",
     title: "Paddy Cultivation: Our way of growing",
+    startTime: "15 December 2021",
     description: (
       <Description
         data={[
           "Grow crop with proven traditional practices",
-          "Collect Gin Berry plants (Gonji chettu) from nearby places and move them to beds",
+          "Collect Gin Berry plant (Gonji chettu) stems from nearby places and move them to beds",
           "Spread the Gin Berry plants across beds and ride the tractor",
           "Follows Transplantation, Field Maintenance, Harvesting, Threshing, Winnowing, and Milling",
           "River water is used for the paddy, which adds additional nutritional value",
           "Crop rotation, to optimize nutrients in the soil",
           "No to anything which contaminates soil, air, water, and the food",
+          "Type: Sugger less, Start date: 15 December 2021, Age:" +
+            getDays("15 December 2021"),
         ]}
       />
     ),
-    src:
-      "https://www.youtube.com/embed/playlist?list=PLtaoilwXUzaI-kKWYQeeqU-0zO1SoOH5f&rel=0",
+    src: "https://www.youtube.com/embed/7IyxYKPqkBI?rel=0",
   },
 ]
 
